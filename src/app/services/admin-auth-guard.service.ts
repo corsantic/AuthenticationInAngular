43b9b0582c146a968ccb34260538f2aed3c90adc
corsantic <aydofurkan25@gmail.com>
2017-11-20 10:03:30 +0300

    api order

--- src/app/services/admin-auth-guard.service.ts
+++ src/app/services/admin-auth-guard.service.ts
@@ -3,22 +3,22 @@ import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class AdminAuthGuard  implements CanActivate{
+export class AdminAuthGuard implements CanActivate {
 
 
 
   constructor(
-    private router:Router,
-    private authService:AuthService
+    private router: Router,
+    private authService: AuthService
   ) {
-   }
+  }
 
-   canActivate(){
-     let user=this.authService.currentUser;
+  canActivate() {
+    let user = this.authService.currentUser;
     if (user && this.authService.currentUser.admin) return true;
 
     this.router.navigate(['/no-access']);
     return false;
 
-   }
+  }
 }
